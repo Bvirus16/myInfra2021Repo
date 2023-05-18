7243510b631e2c5f8baddf944ee364bb9530ade5
Bvirus16 <97404482+Bvirus16@users.noreply.github.com>
2023-05-18 17:21:24 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-dev-tf-bucket"
+    bucket = "my-dev-uterraform-bucket"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
